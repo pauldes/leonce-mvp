8d1ffcc5a9a87df796e184edd4d0cdbbf6cbcb17
pauldes <pauldesigaud@outlook.fr>
2020-09-06 21:19:55 +0200

    First API structure

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 import os
 import sys
 
+from fastapi import FastAPI
+
 from src.scrapping import youtube_channel_scrapper
 
-def main():
+app = FastAPI()
+
+#api = FastAPI(title=config.API_PROJECT_NAME, openapi_url="/api/v1/openapi.json")
+#api.mount("/static", StaticFiles(directory="static"))
+
+@app.on_event("startup")
+async def startup_event():
+    pass
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    pass
+
+@app.get("/")
+async def root():
+    return {"message": "Leonce is the real MVP."}
+
+def test():
     channel = "TrashTalkProduction"
     scrapper = youtube_channel_scrapper.YoutubeChannelScrapper(channel)
     #results = scrapper.get_channel_videos(scrolls=50)
@@ -12,8 +31,3 @@ def main():
     first = results[0]
     print(first)
     print(first.to_dict())
-
-if __name__ == "__main__":
-    sys.exit(main())
-
-
