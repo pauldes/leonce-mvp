80032c4b0da4aa3e067743039b82ee684801cf00
pauldes <pauldesigaud@outlook.fr>
2020-09-07 18:58:24 +0200

    Change database url env variable

--- db/database.py
+++ db/database.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-db_url = os.environ['SQLALCHEMY_DATABASE_URL']
+db_url = os.environ['DATABASE_URL']
 
 if db_url.startswith("sqlite"):
     engine = create_engine(
