978726b1ec781cc551d032f3cb82cce5c5b0afa7
pauldes <pauldesigaud@outlook.fr>
2020-09-08 14:23:09 +0200

    Allow upvotes with JS

---
+++ frontend/static/script.js
@@ -0,0 +1,18 @@
+function upvote(video_id) {
+    let data = {}
+    let url =  "/upvote/" + video_id;
+    postRequest(data, url);
+  }
+  
+function postRequest(data, url){ 
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () { 
+        if (xhr.readyState === 4 && xhr.status != 200) {
+            alert(this.responseText); 
+        }
+    };
+    var data = JSON.stringify(data);
+    xhr.send(data); 
+} 
